8f5930b1d249a292edb322592012bee9d21297ed
Becquet <mdevries@domoti.fr>
2020-05-26 10:16:34 +0200

    add bad remaining?

--- js-asynchrone-part-1-vanilla-callbacks-code/16-grouping-callbacks_.js
+++ js-asynchrone-part-1-vanilla-callbacks-code/16-grouping-callbacks_.js
@@ -5,7 +5,7 @@ const fr = 'https://www.tempsl.fr/fr/'
 const nl = 'https://www.ideal-praktijk.nl/nl/'
 
 orderedParallel(
-  [task(1, fr, '7466113'), task(2, fr, '1896117'), task(3, fr, '7462112'), task(1, nl, '7466113'),],
+  [task(fr, ['7466113', '1896117', '7462112']), task(nl, ['7466113'])],
   (err, results) => {
     if (err) {
       console.log(err)
@@ -19,48 +19,68 @@ orderedParallel(
 // --------------------------------------------------------------------
 
 function orderedParallel(tasks, callback) {
-  const results = {}
-  let remaining = tasks.length
+  const results = []
+  // let remaining = tasks.length
+  let remaining = 0
 
   for (const [index, task] of tasks.entries()) {
-    task((err, result, country, ref) => handleResult(index, err, result, country, ref))
+    console.log(`index: ${index}`);
+
+    task((err, country, refs) => {
+      remaining += refs.length
+      handleResult(index, err, country, refs)
+    })
   }
 
-  function handleResult(index, err, result, country, ref) {
-    console.log(`index: ${index}, country:${country}`);
-
+  function handleResult(index, err, country, refs) {
+    console.log(`index: ${index}, country:${country}, refs:${refs}`);
     if (err) {
       callback(err)
       return
     }
+    refs.map((ref, idx) => {
+      https.get(country + 'boutique/pdtRedirect.aspx?idproduit=' + ref, res => {
+        const httpTohttps = res.headers.location.replace(/^http:\/\//i, 'https://');
+        let allChunk = ''
 
-    https.get(country + 'boutique/pdtRedirect.aspx?idproduit=' + ref, res => {
-      const httpTohttps = res.headers.location.replace(/^http:\/\//i, 'https://');
-      let allChunk = ''
+        https.get(httpTohttps, res => {
+          res.setEncoding('utf8');
+          res.on('data', data => {
+            allChunk = allChunk + data
+          })
+          res.on('end', () => {
+            const { document } = new JSDOM(allChunk).window;
+            console.log(httpTohttps);
 
-      https.get(httpTohttps, res => {
-        res.setEncoding('utf8');
-        res.on('data', data => {
-          allChunk = allChunk + data
+            // results[remaining] = {
+            console.log(`country:${country}`);
+            if (index === 0) {
+              console.log(ref);
+
+              results[remaining - 1] = {
+                // id: idx,
+                ['pk' + idx]: {
+                  link: httpTohttps,
+                  lib: document.querySelector('#ctl00_ContentPlaceHolder1_LB_TITRE_PRODUIT').textContent,
+                  acc: document.querySelector('#ctl00_ContentPlaceHolder1_LB_SOUS_TITRE_PRODUIT').textContent,
+                  price: document.querySelector('#ctl00_ContentPlaceHolder1_LAB_PRIX_PRODUIT').textContent
+                }
+              }
+            }
+
+            // console.log(`remaining: ${remaining}, coutry:${country}, idx:${idx}`);
+
+            if (--remaining === 0) {
+              // callback(null, { fr: results })
+              callback(null, results)
+            }
+
+          })
         })
-        res.on('end', () => {
-          const { document } = new JSDOM(allChunk).window;
-          results[index] = {
-            id: result,
-            link: httpTohttps,
-            lib: document.querySelector('#ctl00_ContentPlaceHolder1_LB_TITRE_PRODUIT').textContent,
-            acc: document.querySelector('#ctl00_ContentPlaceHolder1_LB_SOUS_TITRE_PRODUIT').textContent,
-            price: document.querySelector('#ctl00_ContentPlaceHolder1_LAB_PRIX_PRODUIT').textContent
-          }
-          if (--remaining === 0) {
-            // callback(null, { fr: results })
-            callback(null, results)
-          }
 
-        })
-      })
+      }).on('error', (e) => callback(e.message));
+    })
 
-    }).on('error', (e) => callback(e.message));
 
     // On les range bien à leur position définie.
 
@@ -69,7 +89,7 @@ function orderedParallel(tasks, callback) {
 
 // --------------------------------------------------------------------
 
-function task(id, country, ref) {
+function task(index, country, refs) {
   // return (cb) => setTimeout(() => cb(null, id), delay)
-  return (cb) => cb(null, `pk${id}`, country, ref)
+  return (cb) => cb(index, null, country, refs)
 }
